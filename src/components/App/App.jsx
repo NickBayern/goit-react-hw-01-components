e0d3b746ffb5d40c47259f4ea689ac8a4c3c3b87
Nick Bayern <jeldak@icloud.com>
2022-12-13 23:09:43 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,24 @@
+import { Profile } from "../Profile/Profile";
+import { Statistics } from "components/Statistics/Statistics";
+import { FriendsList } from "components/FriendList/FriendsList";
+import { TransactionHistory } from "components/Transactions/Transactions";
+import { Container } from "./App.styled";
+import user from '../user.json';
+import data from '../data.json';
+import friends from '../friends.json';
+import transactions from '../transactions.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+    <Profile username={user.username}
+             tag={user.tag}
+             location={user.location}
+             avatar={user.avatar}
+             stats={user.stats}/>
+    <Statistics title="Upload stats" stats={data}/>   
+    <FriendsList data={friends}/>  
+    <TransactionHistory data={transactions}/>
+    </Container>
   );
 };
